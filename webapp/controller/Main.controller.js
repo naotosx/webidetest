7011a5f6fa73ee3c38c70a7a3ebe9a3acba811e8
Amari Naoto <naotoamari@gmail.com>
2018-05-04 17:53:41 +0000

    asd

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,14 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"
+], function(Controller, MessageToast) {
 	"use strict";
 
 	return Controller.extend("git.test.WebIDETestGit.controller.Main", {
+
+		onOk: function() {
+			MessageToast.show("Okeyking");
+		}
 
 	});
 });
